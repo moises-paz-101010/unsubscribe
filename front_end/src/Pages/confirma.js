ef17a32af41bcbcf42e13010c1a238b2b9f422c4
moises <moisespaz678@gmail.com>
2025-06-17 15:44:08 -0400

    configuracao de rotas no front end

---
+++ front_end/src/Pages/confirma.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Comfirma(){
+    return (
+        <React.Fragment>
+            <h1>
+                Segunda pagina.
+            </h1>
+        </React.Fragment>
+    );
+}
+
+export default Comfirma;
---
+++ front_end/src/Pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home(){
+    return (
+        <React.Fragment>
+            <h1>
+                Página principal
+            </h1>
+        </React.Fragment>
+    )
+}
+export default Home;
